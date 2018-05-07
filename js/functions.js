1c863b350370d23190f7bd5f3c91d38b82c83300
nilanka manoj <nilankaeng16@gmail.com>
2018-05-07 12:41:41 +0530

    validations added

--- js/functions.js
+++ js/functions.js
@@ -1,68 +1,70 @@
 
-		var signin = "<div id ='frmsignin'>"
-			+"<h3 class='cursive-font'>sign in</h3>"
-			+"<form action='javascript:signIn()'>"
-			+"<div class='row form-group'>"
-			+"<div class='col-md-12'>"
-			+"<label for='email'>email</label>"
-			+"<input type='text' id='email' class='form-control'>"
-			+"</div>"
-			+"</div>"
-			+"<div class='row form-group'>"
-			+"<div class='col-md-12'>"
-			+"<label for='password'>password</label>"
-			+"<input type='password' id='password' class='form-control'>"
-			+"</div>"
-			+"</div>"
-	  	+"<div class='row form-group'>"
-			+"<div class='col-md-12'>"
-			+"<button class='btn btn-primary' id ='btnsin' ><span>sign in</span></a></button>"
-			+"</div>"
-			+"</div>"
-		  +"</form>"
-		  +"</div>";
+var signin = "<div id ='frmsignin'>"
+	+ "<h3 class='cursive-font'>sign in</h3>"
+	+ "<div id ='info'></div>"
+	+ "<form action='javascript:signIn()'>"
+	+ "<div class='row form-group'>"
+	+ "<div class='col-md-12'>"
+	+ "<label for='email'>email</label>"
+	+ "<input type='text' id='email' class='form-control'>"
+	+ "</div>"
+	+ "</div>"
+	+ "<div class='row form-group'>"
+	+ "<div class='col-md-12'>"
+	+ "<label for='password'>password</label>"
+	+ "<input type='password' id='password' class='form-control'>"
+	+ "</div>"
+	+ "</div>"
+	+ "<div class='row form-group'>"
+	+ "<div class='col-md-12'>"
+	+ "<button class='btn btn-primary' id ='btnsin' ><span>sign in</span></a></button>"
+	+ "</div>"
+	+ "</div>"
+	+ "</form>"
+	+ "</div>";
 
-			var signup = "<div id ='frmsignup'>"
-				+"<h3 class='cursive-font'>sign up</h3>"
-				+"<form action='javascript:signUp()'>"
-				+"<div class='row form-group'>"
-				+"<div class='col-md-12'>"
-				+"<label for='email'>email</label>"
-				+"<input type='text' id='email' class='form-control'>"
-				+"</div>"
-				+"</div>"
-				+"<div class='row form-group'>"
-				+"<div class='col-md-12'>"
-				+"<label for='password'>password</label>"
-				+"<input type='password' id='password' class='form-control'>"
-				+"</div>"
-				+"</div>"
-				+"<div class='row form-group'>"
-				+"<div class='col-md-12'>"
-				+"<label for='passwordconf'>password confirm</label>"
-				+"<input type='password' id='passwordconf' class='form-control'>"
-				+"</div>"
-				+"</div>"
-		  	+"<div class='row form-group'>"
-				+"<div class='col-md-12'>"
-				+"<button class='btn btn-primary' id ='btnsup'><span>sign up</span></a></button>"
-				+"</div>"
-				+"</div>"
-			  +"</form>"
-			  +"</div>";
+var signup = "<div id ='frmsignup'>"
+	+ "<h3 class='cursive-font'>sign up</h3>"
+	+ "<div id ='info'></div>"
+	+ "<form action='javascript:signUp()'>"
+	+ "<div class='row form-group'>"
+	+ "<div class='col-md-12'>"
+	+ "<label for='email'>email</label>"
+	+ "<input type='text' id='email' class='form-control'>"
+	+ "</div>"
+	+ "</div>"
+	+ "<div class='row form-group'>"
+	+ "<div class='col-md-12'>"
+	+ "<label for='password'>password</label>"
+	+ "<input type='password' id='password' class='form-control'>"
+	+ "</div>"
+	+ "</div>"
+	+ "<div class='row form-group'>"
+	+ "<div class='col-md-12'>"
+	+ "<label for='passwordconf'>password confirm</label>"
+	+ "<input type='password' id='passwordconf' class='form-control'>"
+	+ "</div>"
+	+ "</div>"
+	+ "<div class='row form-group'>"
+	+ "<div class='col-md-12'>"
+	+ "<button class='btn btn-primary' id ='btnsup'><span>sign up</span></a></button>"
+	+ "</div>"
+	+ "</div>"
+	+ "</form>"
+	+ "</div>";
 
 
 
 
-	document.getElementById('frm').innerHTML = signin;
-	var state = 2;
-	document.getElementById("alter").onclick = function () {
-		if(state == 1){
-			state = 2;
-			document.getElementById('frm').innerHTML = signin;
+document.getElementById('frm').innerHTML = signin;
+var state = 2;
+document.getElementById("alter").onclick = function () {
+	if (state == 1) {
+		state = 2;
+		document.getElementById('frm').innerHTML = signin;
 
 	}
-	else{
+	else {
 		state = 1;
 		document.getElementById('frm').innerHTML = signup;
 
@@ -70,40 +72,70 @@
 };
 
 function signIn() {
-	var data = "email="+document.getElementById('email').value+"&password="+document.getElementById('password').value;
+	if (!document.getElementById('email').value || !document.getElementById('password').value) {
+		document.getElementById('info').innerHTML = "<div class=''><i class='glyphicon glyphicon-warning-sign'></i> &nbsp;all fields required! </div>";
+	}
+	else if (!validateEmail(document.getElementById('email').value)) {
+		document.getElementById('info').innerHTML = "<div class=''><i class='glyphicon glyphicon-warning-sign'></i> &nbsp;invalid email! </div>";
+	}
+	else if (document.getElementById('password').value.length < 6) {
+		document.getElementById('info').innerHTML = "<div class=''><i class='glyphicon glyphicon-warning-sign'></i> &nbsp;pasword required more than 6! </div>";
+	}
+	else {
+		var data = "email=" + document.getElementById('email').value + "&password=" + document.getElementById('password').value;
 
-	var xhr = new XMLHttpRequest();
-	//xhr.withCredentials = true;
+		var xhr = new XMLHttpRequest();
+		//xhr.withCredentials = true;
 
-	xhr.addEventListener("readystatechange", function () {
-	  if (this.readyState === 4) {
-	    console.log(this.responseText);
-	  }
-	});
+		xhr.addEventListener("readystatechange", function () {
+			if (this.readyState === 4) {
+				console.log(this.responseText);
+			}
+		});
 
-	xhr.open("POST", "https://adaptyoumain.herokuapp.com/api/authenticate");
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	//xhr.setRequestHeader("Cache-Control", "no-cache");
+		xhr.open("POST", "https://adaptyoumain.herokuapp.com/api/authenticate");
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		//xhr.setRequestHeader("Cache-Control", "no-cache");
 
-	xhr.send(data);
+		xhr.send(data);
+	}
 }
 
-function signUp() {
-	var data = "email="+document.getElementById('email').value+"&password="+document.getElementById('password').value;
+	function signUp() {
+		if (!document.getElementById('email').value || !document.getElementById('password').value || !document.getElementById('passwordconf').value) {
+			document.getElementById('info').innerHTML = "<div class=''><i class='glyphicon glyphicon-warning-sign'></i> &nbsp;all fields required! </div>";
+		}
+		else if (!validateEmail(document.getElementById('email').value)) {
+			document.getElementById('info').innerHTML = "<div class=''><i class='glyphicon glyphicon-warning-sign'></i> &nbsp;invalid email! </div>";
+		}
+		else if (document.getElementById('password').value.length < 6) {
+			document.getElementById('info').innerHTML = "<div class=''><i class='glyphicon glyphicon-warning-sign'></i> &nbsp;pasword required more than 6! </div>";
+		}
+		else if (document.getElementById('password').value != document.getElementById('passwordconf').value) {
+			document.getElementById('info').innerHTML = "<div class=''><i class='glyphicon glyphicon-warning-sign'></i> &nbsp;password and confirm mismatch! </div>";
+		}
+		else {
+			var data = "email=" + document.getElementById('email').value + "&password=" + document.getElementById('password').value;
 
-	var xhr = new XMLHttpRequest();
-	//xhr.withCredentials = true;
+			var xhr = new XMLHttpRequest();
+			//xhr.withCredentials = true;
 
-	xhr.addEventListener("readystatechange", function () {
-	  if (this.readyState === 4) {
-	    console.log(this.responseText);
-	  }
-	});
+			xhr.addEventListener("readystatechange", function () {
+				if (this.readyState === 4) {
+					console.log(this.responseText);
+				}
+			});
 
-	xhr.open("POST", "https://adaptyoumain.herokuapp.com/api/signup");
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	//xhr.setRequestHeader("Cache-Control", "no-cache");
+			xhr.open("POST", "https://adaptyoumain.herokuapp.com/api/signup");
+			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			//xhr.setRequestHeader("Cache-Control", "no-cache");
 
-	xhr.send(data);
+			xhr.send(data);
+		}
 
-};
+	}
+
+	function validateEmail(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+	}
