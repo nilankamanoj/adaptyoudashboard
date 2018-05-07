b3d94bb93e2d39d3ec1c6b30a3774b6537434728
nilanka manoj <nilankaeng16@gmail.com>
2018-05-07 20:27:47 +0530

    doc page added

---
+++ js/documentation.js
@@ -0,0 +1,61 @@
+if (document.cookie) {
+    var cred = document.cookie.split(';');
+    var email = cred[0].split("=")[1];
+    var token = cred[1].split("=")[1];
+    loadContent();
+}
+
+
+
+else {
+    document.location.replace("index.html");
+}
+
+
+function loadContent() {
+
+
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            try {
+                var res = JSON.parse(this.responseText);
+                if (res["success"]) {
+                    console.log(this.responseText);
+                    
+                }
+                else {
+                    delete document.cookie;
+                    document.location.replace("index.html");
+                }
+            }
+            catch (e) {
+                delete document.cookie;
+                document.location.replace("index.html");
+            }
+
+        }
+    });
+
+    xhr.open("GET", "https://adaptyoumain.herokuapp.com/api/memberinfo");
+    xhr.setRequestHeader("Authorization", token);
+
+
+
+    xhr.send(data);
+
+    document.getElementById("alter").onclick = function () {
+        logOut();
+    };
+
+    document.getElementById("message").innerHTML = "Welcome " + email;
+}
+function logOut() {
+    document.cookie = "email=" + "" + "; path=/";
+    document.cookie = "token=" + "" + "; path=/";
+    document.location.replace("index.html");
+}
